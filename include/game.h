90135e426d5694b785e26fa3dc87524bf7e2c12e
Ben Thacher <8000ice@gmail.com>
2024-05-16 17:35:20 -0400

    Updated game loop to keep current state of pointer/buttons, fixed event emitting

--- include/game.h
+++ include/game.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include "entity.h"
+#include "mathutils.h"
 
 #define FPS                60
 #define DT                 (1.0 / FPS)
@@ -21,3 +22,7 @@ void game_stop_loop();
 
 void game_add_entity(struct entity *entity);
 void game_remove_entity(struct entity *entity);
+
+enum button_state game_get_button(enum button button);
+void game_get_pointer_position(struct vec2 *position);
+void game_get_pointer_velocity(struct vec2 *velocity);
--- src/game.c
+++ src/game.c
@@ -6,9 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "entity.h"
 #include "audio.h"
+#include "pal.h"
+#include "mathutils.h"
 
 struct entity_list_node {
     struct entity *entity;
@@ -19,6 +22,30 @@ struct entity_list_node {
 static double frame_start, frame_duration;
 static bool running = false;
 static struct entity_list_node *entity_list_head = NULL;
+
+static struct pointer {
+    struct vec2 current_position;
+    enum pointer_state current_state;
+    struct vec2 previous_position;
+    struct vec2 velocity;
+    struct vec2 dragging_entity_offset;
+    struct entity *dragging_entity;
+    bool previous_position_valid;
+    bool can_click_entity;
+
+    double previous_time;
+} pointer = {
+    .previous_position_valid = false,
+    .can_click_entity = true,
+    .dragging_entity = NULL
+};
+
+static enum button_state buttons[NUM_BUTTONS] = {
+    [BUTTON_LEFT] = BUTTON_STATE_UP,
+    [BUTTON_RIGHT] = BUTTON_STATE_UP,
+    [BUTTON_LEFT_TRIGGER] = BUTTON_STATE_UP,
+    [BUTTON_RIGHT_TRIGGER] = BUTTON_STATE_UP,
+};
 
 void game_stop_loop() {
     running = false;
@@ -61,20 +88,21 @@ void game_remove_entity(struct entity *entity) {
 
 static void emit_events() {
     struct pal_event e;
+    double current_time = pal_get_time();
 
     while (pal_poll_event(&e)) {
         switch (e.type) {
             case PAL_EVENT_TYPE_BUTTON:
                 for (struct entity_list_node *node = entity_list_head; node != NULL; node = node->next) {
-                    entity_event_emit(node->entity, ENTITY_EVENT_BUTTON, &e.button, sizeof(struct button_event));
+                    entity_event_emit(node->entity, e.button.state == BUTTON_STATE_UP ? ENTITY_EVENT_BUTTON_UP : ENTITY_EVENT_BUTTON_DOWN, &e.button.which, sizeof(enum button));
                 }
+                buttons[e.button.which] = e.button.state;
                 break;
 
             case PAL_EVENT_TYPE_POINTER:
-                for (struct entity_list_node *node = entity_list_head; node != NULL; node = node->next) {
-                    // check pointer collisions
-                    // entity_event_emit(node->entity, ENTITY_EVENT_BUTTON, &e.button, sizeof(struct button_event));
-                }
+                pointer.current_position = e.pointer.position;
+                pointer.current_state = e.pointer.state;
+
                 break;
 
             case PAL_EVENT_TYPE_IMU:
@@ -83,9 +111,79 @@ static void emit_events() {
         }
     }
 
-    for (struct entity_list_node *node = entity_list_head; node != NULL; node = node->next) {
-        entity_event_emit(node->entity, ENTITY_EVENT_UPDATE, NULL, 0);
+    // emit pointer events for entities that the pointer interacted with
+    if (pointer.current_state == POINTER_STATE_UP) {
+        // release dragging entity if one exists
+        if (pointer.dragging_entity) {
+            entity_event_emit(pointer.dragging_entity, ENTITY_EVENT_DRAG_STOP, NULL, 0);
+            entity_state_clear(pointer.dragging_entity, ENTITY_STATE_DRAGGING);
+            pointer.dragging_entity->phys.velocity = pointer.velocity;
+        }
+
+        pointer.dragging_entity = NULL;
+
+        for (struct entity_list_node *node = entity_list_head; node != NULL; node = node->next) {
+            if (entity_state_check(node->entity, ENTITY_STATE_CLICKED)) {
+                entity_event_emit(node->entity, ENTITY_EVENT_RELEASE, NULL, 0);
+                entity_state_clear(node->entity, ENTITY_STATE_CLICKED);
+            }
+        }
+
+        pointer.can_click_entity = true;
+        pointer.previous_position_valid = false;
+    } else if (pointer.current_state == POINTER_STATE_DOWN) {
+        if (pointer.dragging_entity != NULL) {
+            // entity is already being dragged, update the entity's position
+            vec2_add(&pointer.current_position, &pointer.dragging_entity_offset, &pointer.dragging_entity->phys.position);
+        } else if (pointer.can_click_entity) {
+            for (struct entity_list_node *node = entity_list_head; node != NULL; node = node->next) {
+                if (!physics_check_point_collision(&node->entity->phys, &pointer.current_position))
+                    continue;
+
+                if (entity_state_check(node->entity, ENTITY_STATE_DRAGGABLE) && pointer.dragging_entity == NULL) {
+                    // Drag start!!!
+                    pointer.dragging_entity = node->entity;
+
+                    vec2_sub(&pointer.dragging_entity->phys.position, &pointer.current_position, &pointer.dragging_entity_offset);
+                    entity_event_emit(pointer.dragging_entity, ENTITY_EVENT_DRAG_START, &pointer.dragging_entity_offset, sizeof(pointer.dragging_entity_offset));
+                    entity_state_set(pointer.dragging_entity, ENTITY_STATE_DRAGGING);
+
+                    pointer.dragging_entity->phys.velocity.x = pointer.dragging_entity->phys.velocity.y = 0.0;
+                }
+
+                entity_event_emit(node->entity, ENTITY_EVENT_CLICK, NULL, 0);
+                entity_state_set(node->entity, ENTITY_STATE_CLICKED);
+            }
+
+            pointer.can_click_entity = false;
+        }
+
+        if (pointer.previous_position_valid) {
+            pointer.velocity.x = (pointer.current_position.x - pointer.previous_position.x) / (current_time - pointer.previous_time);
+            pointer.velocity.y = (pointer.current_position.y - pointer.previous_position.y) / (current_time - pointer.previous_time);
+        } else {
+            pointer.velocity.x = 0.0;
+            pointer.velocity.y = 0.0;
+        }
+
+        pointer.previous_position = pointer.current_position;
+        pointer.previous_time = current_time;
+        pointer.previous_position_valid = true;
     }
+}
+
+enum button_state game_get_button(enum button button) {
+    return buttons[button];
+}
+
+void game_get_pointer_position(struct vec2 *position) {
+    position->x = pointer.current_position.x;
+    position->y = pointer.current_position.y;
+}
+
+void game_get_pointer_velocity(struct vec2 *velocity) {
+    velocity->x = pointer.velocity.x;
+    velocity->y = pointer.velocity.y;
 }
 
 static void render_all() {
@@ -97,17 +195,17 @@ static void render_all() {
 
 static void update_all(float dt) {
     for (struct entity_list_node *e = entity_list_head; e != NULL; e = e->next) {
-        if (e->entity->config_flags.do_physics)
+        if (e->entity->_event_handlers[ENTITY_EVENT_UPDATE])
+            e->entity->_event_handlers[ENTITY_EVENT_UPDATE](e->entity, (void *) &dt);
+
+        if (entity_state_check(e->entity, ENTITY_STATE_DO_PHYSICS))
             physics_integrate(&e->entity->phys, dt);
     }
 }
 
 void entity_handle_all_events() {
     for (struct entity_list_node *node = entity_list_head; node != NULL; node = node->next) {
-        for (int e = 0; e < NUM_ENTITY_EVENTS; e++) {
-            if (entity_event_check(node->entity, e))
-                entity_call_event_handler(node->entity, (enum entity_event) e);
-        }
+        entity_handle_pending_events(node->entity);
     }
 }
 
@@ -123,9 +221,9 @@ void game_run_loop() {
 
         emit_events();
 
-        update_all(DT);
-
         entity_handle_all_events();
+
+        update_all(DT);
 
         // render
         render_all();
